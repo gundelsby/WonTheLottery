d9f9f39e96bb91d8a704faca2fc4e690ea7b94d5
Ola Christian Gundelsby <ola-christian.gundelsby@telenor.com>
2013-05-02 16:20:42 +0200

    completed init skeleton

--- js/wonthelottery.js
+++ js/wonthelottery.js
@@ -3,37 +3,49 @@
  */
 var lotto = lotto || ( function () {
 	var NUMBERS_IN_ROW = 7;
+	var ADDITIONAL_NUMBERS = 3;
 	var ROWS_IN_TICKET = 10;
+	
+	function createTableRow(numCells) {
+		var i, row, numbers = [];
+		var inputAttributes = {
+			"type": "number",
+			"autocomplete": "off",
+			"min": "1",
+			"max": "34"
+		};
+		for(i = 0; i < numCells; i++){
+			numbers.push(vermin.dom.create("td", {}, vermin.dom.create("input", inputAttributes)));
+		}
+		return vermin.dom.create("tr", {}, numbers);
+	};
+	
 	var lotto = {
 		init: function() {
+			var self = this;
+			vermin.elements.byId("save_ticket").addEventListener("click", (function() { return self.saveTicket}()), false);
+			vermin.elements.byId("check_results").addEventListener("click", (function() { return self.checkResults}()), false);
 			this.createTicket();
 			this.buildDrawSection();
 		},
 		createTicket: function() {
-			var i, j, row, rows = [], numbers = [];
-			var inputAttributes = {
-				"type": "number",
-				"autocomplete": "off",
-				"min": "1",
-				"max": "34"
-			}
+			var i;
 			this.tableTicket = vermin.elements.byId("ticket");
 			for(i = 0; i < ROWS_IN_TICKET; i++){
-				for(j = 0; j < NUMBERS_IN_ROW; j++){
-					numbers.push(vermin.dom.create("td", {}, vermin.dom.create("input", inputAttributes)));
-				}
-				row = vermin.dom.create("tr", {}, numbers);
-				rows.push(row);
-				numbers = [];
-			};
-			for(i = 0; i < rows.length; i++){
-				this.tableTicket.appendChild(rows[i]);
+				this.tableTicket.appendChild(createTableRow(NUMBERS_IN_ROW));
 			};
 			
 		},
 		buildDrawSection: function() {
-			var tableMainNumbers = vermin.elements.byId("numbers_main");
-			var tableAddNumbers = vermin.elements.byId("numbers_additional");
+			var drawSectionContainer = vermin.elements.byId("drawdata");
+			drawSectionContainer.appendChild(vermin.dom.create("table", {"id": "main_numbers"}, createTableRow(NUMBERS_IN_ROW)));
+			drawSectionContainer.appendChild(vermin.dom.create("table", {"id": "additional_numbers"}, createTableRow(ADDITIONAL_NUMBERS)));
+		},
+		checkResults: function () {
+			console.log("checkResults()");
+		},
+		saveTicket: function () {
+			console.log("saveTicket()");
 		}
 	};
 	return lotto;
