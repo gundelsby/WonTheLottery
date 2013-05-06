e97e7b5abca2e4ac9d46d7f65d46eb381d217667
Ola Christian Gundelsby <lug00ber@gmail.com>
2013-05-06 02:14:33 +0200

    Completed ticket storage, but not reading from storage during init

--- js/wonthelottery.js
+++ js/wonthelottery.js
@@ -5,7 +5,9 @@ var lotto = lotto || ( function () {
 	var NUMBERS_IN_ROW = 7;
 	var ADDITIONAL_NUMBERS = 3;
 	var ROWS_IN_TICKET = 10;
-	var LOCAL_STORAGE_PREFIX = "WonTheLottery";
+	var LOCAL_STORAGE_PREFIX = "WonTheLottery:";
+	var LOCAL_STORAGE_TICKET_OBJECT_KEY = "ticket";
+	var STORED_OBJECT_ROWS_KEY = "rows";
 	
 	function createTableRow(numCells) {
 		var i, row, numbers = [];
@@ -21,16 +23,26 @@ var lotto = lotto || ( function () {
 		return vermin.dom.create("tr", {}, numbers);
 	};
 	
+	function extractTableRowData(tableRow) {
+		var i, values = [];
+		var tableCells = tableRow.getElementsByTagName("input");
+		for(i = 0; i < tableCells.length; i++) {
+			values.push(tableCells[i].value);
+		}
+		return values;
+	};
+	
 	var lotto = {
 		init: function() {
-			var self = this;
-			vermin.elements.byId("save_ticket").addEventListener("click", (function() { return self.saveTicket}()), false);
-			vermin.elements.byId("check_results").addEventListener("click", (function() { return self.checkResults}()), false);
+			vermin.elements.byId("save_ticket").addEventListener("click", this.saveTicket.bind(this), false);
+			vermin.elements.byId("check_results").addEventListener("click", this.checkResults.bind(this), false);
 			this.createTicket();
 			this.buildDrawSection();
 		},
 		createTicket: function() {
-			var i;
+			var i, savedTicket, ticketRows;
+			savedTicket = localStorage.getItem(LOCAL_STORAGE_PREFIX + LOCAL_STORAGE_TICKET_OBJECT_KEY);
+			ticketRows = savedTicket !== undefined || !savedTicket ? JSON.parse(savedTicket) : null;
 			this.tableTicket = vermin.elements.byId("ticket");
 			for(i = 0; i < ROWS_IN_TICKET; i++){
 				this.tableTicket.appendChild(createTableRow(NUMBERS_IN_ROW));
@@ -46,9 +58,13 @@ var lotto = lotto || ( function () {
 			console.log("checkResults()");
 		},
 		saveTicket: function () {
+			var i, rows = [], ticket = {};
 			var tableRows = this.tableTicket.getElementsByTagName("tr");
-			console.log("rows: ", tableRows);
-			console.log("saveTicket()");
+			for(i = 0; i < tableRows.length; i++) {
+				rows.push(extractTableRowData(tableRows[i]));
+			}
+			ticket[STORED_OBJECT_ROWS_KEY] = rows;
+			localStorage.setItem(LOCAL_STORAGE_PREFIX + LOCAL_STORAGE_TICKET_OBJECT_KEY, JSON.stringify(ticket));
 		}
 	};
 	return lotto;
